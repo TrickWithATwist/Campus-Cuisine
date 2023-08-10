9a9af4ec0caa59b2fc79be5607762ea82cb54d4c
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-10 11:43:41 -0400

    added all icons to custom tab bar

--- Campus Cuisine/Campus Cuisine/ContentView.swift
+++ Campus Cuisine/Campus Cuisine/ContentView.swift
@@ -17,13 +17,25 @@ struct ContentView: View {
         {
             //establishing tab items
             /* Each tab item name is printed and the other details such as icons and other attributes are added in as modifiers*/
+            //home
             Text("Home").tabItem()
             {
                 Label("Home", systemImage: "house")
             }
+            //search
             Text("Search").tabItem()
             {
                 Label("Search", systemImage: "fork.knife.circle")
+            }
+            //map
+            Text("Map").tabItem()
+            {
+                Label("Map", systemImage: "map")
+            }
+            //Star
+            Text("Star").tabItem()
+            {
+                Label("Favorites", systemImage: "star")
             }
             
         }
