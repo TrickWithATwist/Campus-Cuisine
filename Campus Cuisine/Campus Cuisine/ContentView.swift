dcd1fb4a37814d96e766b90f0268f9b5c3769ad2
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-10 09:25:56 -0400

    i changed two files

--- Campus Cuisine/Campus Cuisine/ContentView.swift
+++ Campus Cuisine/Campus Cuisine/ContentView.swift
@@ -7,54 +7,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        /*creating button to go to other view file
-         by establishing NavigationView and setting
-         up button properities*/
-        NavigationView
-        {
-            VStack()
-            {
-                /*Creating butto with string protocal
-                 "Search." When it is tapped the
-                 view will change to View 2*/
-                NavigationLink("Tap for screen 2")
-                {
-                    View2()
-                }
-                
-                //printing text to label screen
-                //temp for dev
-                Text("this is screen 1")
-            }.padding(.all).background(Color.red)
-                .toolbar
-            {
-                /* Established toolbar attribute to the V stack and Created list of items and the brakckets of each item contain their attributes i.e. Image appearance, etc*/
-                //creating gear item
-                ToolbarItem(placement: .bottomBar)
-                {
-                    Image(systemName: "gear")
-                        .frame(alignment: .bottomTrailing)
-                }
-                //creating house item
-                ToolbarItem(placement: .bottomBar)
-                {
-                    Image(systemName: "house.fill")
-                        .frame(alignment: .bottomTrailing)
-                }
-                //creating profile icon item
-                ToolbarItem(placement: .bottomBar)
-                {
-                    Image(systemName: "star.fill")
-                        .frame(alignment: .bottomTrailing)
-                }
-
-            }
-            //temporarly adding padding and color
-        }
+    var body: some View
+    {
+        /*creating Tab bar or whatever*/
+        Text("hello")
         
     }
-}
+        
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
