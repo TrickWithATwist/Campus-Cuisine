f23e72e67281a878897ef4039deee53076f7fcb0
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-10 07:10:10 -0400

    Made new project, will make better framework

---
+++ Campus Cuisine/Campus Cuisine/Campus_CuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_CuisineApp.swift
+//  Campus Cuisine
+//
+//  Created by Akram Djomehri on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Campus_CuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
