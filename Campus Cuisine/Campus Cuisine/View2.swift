97029bba8f4ff37b5cbf7e98616442ae76ce650b
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-10 07:40:46 -0400

    adding two other views

---
+++ Campus Cuisine/Campus Cuisine/View2.swift
@@ -0,0 +1,20 @@
+//
+//  View2.swift
+//  Campus Cuisine
+//
+//  Created by Akram Djomehri on 8/10/23.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View2_Previews: PreviewProvider {
+    static var previews: some View {
+        View2()
+    }
+}
---
+++ Campus Cuisine/Campus Cuisine/View3.swift
@@ -0,0 +1,20 @@
+//
+//  View3.swift
+//  Campus Cuisine
+//
+//  Created by Akram Djomehri on 8/10/23.
+//
+
+import SwiftUI
+
+struct View3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View3_Previews: PreviewProvider {
+    static var previews: some View {
+        View3()
+    }
+}
