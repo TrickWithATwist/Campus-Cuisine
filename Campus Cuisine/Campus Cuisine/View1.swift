32a32fa3700ad42b924b4ce18b70b36c3f8b65ec
colelanzy <lanzing2@msu.edu>
2023-08-11 11:11:25 -0400

    Added some buttons to Home Screen
    Not formatted correctly yet.

--- Campus Cuisine/Campus Cuisine/View1.swift
+++ Campus Cuisine/Campus Cuisine/View1.swift
@@ -15,6 +15,8 @@ struct View1: View {
             /* adding background by establishing a color and having it be overlayed over all the other content on the view*/
             Color.black.ignoresSafeArea().overlay(
                 //content that the color is overlaying
+                
+                // Vertical Stack holding contents of Home Screen
                 VStack
                 {
                     // "Categories" title and "See All"
@@ -27,16 +29,46 @@ struct View1: View {
                         /* creating navigation link between home view and see all view*/
                         NavigationLink("See all", destination: SeeAllView()).foregroundColor(.white).font(.system(size: 25)).frame(maxWidth: .infinity, maxHeight: 670, alignment: .topTrailing)
                     }
+                    
+                    // Fast Food Button
+                    Button(action: { }, label: {
+                      Text("Fast Food")
+                        .padding(50)
+                        .background(Color.orange)
+                        .cornerRadius(8)
+                        .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading)
+                    })
+                    
                     // "Cultures" title
                     HStack
                     {
                         Text("Cultures").foregroundColor(.white).font(.system(size: 40)).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                     }
+                    
+                    // Asian Button
+                    Button(action: { }, label: {
+                        Text("Asian")
+                            .padding(50)
+                            .background(Color.orange)
+                            .cornerRadius(8)
+                            .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading)
+                    })
+                    
                     // "By Food" title
                     HStack
                     {
                         Text("By Food").foregroundColor(.white).font(.system(size: 40)).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                     }
+                    
+                    // Pizza Button
+                    Button(action: { }, label: {
+                        Text("Pizza")
+                            .padding(50)
+                            .background(Color.orange)
+                            .cornerRadius(8)
+                            .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading)
+                    })
+                    
                 })
         }.navigationBarHidden(true) //bar is hidden
     }
