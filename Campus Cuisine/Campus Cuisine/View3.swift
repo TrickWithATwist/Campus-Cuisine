42b913f71abf15935b2840fbebbba2b4ae4c5816
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-11 11:08:17 -0400

    wip commit view 3

--- Campus Cuisine/Campus Cuisine/View3.swift
+++ Campus Cuisine/Campus Cuisine/View3.swift
@@ -11,8 +11,20 @@ import MapKit
 struct View3: View {
     var body: some View
     {
+        /* establishing mutable variable for
+         geographic location of user. @State means
+         that the variable is mutable and "var"
+         is the class type of the variable*/
+        @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: -121, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)),
+        //MKCoordinateRegion establishes rectangular map
+        //reigion in specific latitue and longitude
+        
+        
         //establish backdrop color
         Color.black.ignoresSafeArea().overlay(
+            
+        //establing map
+            Map(coordinateRegion: $region),
         VStack
         {
             Text("Map placeholder").foregroundColor(.white)
