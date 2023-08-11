d0b50502eabfe561a21ca0ea8930be57e2e07e6d
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-11 09:04:29 -0400

    adding see all navigation link

--- Campus Cuisine/Campus Cuisine/View1.swift
+++ Campus Cuisine/Campus Cuisine/View1.swift
@@ -9,15 +9,30 @@ import SwiftUI
 struct View1: View {
     var body: some View
     {
-        /* adding background by establishing a color and having it be overlayed over all the other content on the view*/
-        Color.black.ignoresSafeArea().overlay(
-        //content that the color is overlaying
-        VStack
+        /* establishing navigation view and hiding the bar using a modifier*/
+        NavigationView
         {
-            //adding text for view
-            /* using modifiers to increase text size and color*/
-            Text("Categories").foregroundColor(.white).font(.system(size: 40)).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-        })
+            /* adding background by establishing a color and having it be overlayed over all the other content on the view*/
+            Color.black.ignoresSafeArea().overlay(
+                //content that the color is overlaying
+                VStack
+                {
+                    //adding text for view
+                    /* using modifiers to increase text size and color*/
+                    Text("Categories").foregroundColor(.white).font(.system(size: 40)).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    
+                    /* creating navigation link between home view and see all view*/
+                    NavigationLink("See all", destination: SeeAllView()).foregroundColor(.white).font(.system(size: 25)).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                })
+        }.navigationBarHidden(true) //bar is hidden
+    }
+}
+
+/* establishing extra view for the "See All" screen*/
+struct SeeAllView: View {
+    var body: some View
+    {
+        Text("See all screen")
     }
 }
 
