dbcdd929dd712cf1f8c30d5f279dc712c1c99956
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-14 07:52:42 -0400

    Got app to ask for user permission to use location

--- Campus Cuisine/Campus Cuisine/View3.swift
+++ Campus Cuisine/Campus Cuisine/View3.swift
@@ -46,13 +46,43 @@ final class View3Model: ObservableObject
             if CLLocationManager.locationServicesEnabled()
             {
                 locationmanager = CLLocationManager()
-                locationmanager?.desiredAccuracy = kCLLocationAccuracyBest
+                //locationmanager?.desiredAccuracy = kCLLocationAccuracyBest
             }
             else
             {
                 print("Please turn on location services to use map.")
             }
         }
+    
+    //establishing function to get user authorization
+    func checkLocationauthorization()
+    {
+        guard let locationmanager = locationmanager else
+        {
+            return
+        }
+        
+        //switch statement of authorization status
+        switch locationmanager.authorizationStatus
+        {
+            case .notDetermined:
+                locationmanager.requestWhenInUseAuthorization()
+            case .restricted:
+            print("Your location is currently restricted.")
+        case .denied:
+            print("Your location is currently restricted.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            //breaking in a switch statement in Swift
+            //is kind of like just putting "null"
+            //in a statement/block of code
+            break
+        @unknown default:
+            break
+        }
+        
+    }
         
 }
+
+
 
