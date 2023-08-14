992a94aec1488774f32bde3f47b3dedcd2782252
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-14 07:16:44 -0400

    FINALLY I GOT A MAP TO SHOW

--- Campus Cuisine/Campus Cuisine/View3.swift
+++ Campus Cuisine/Campus Cuisine/View3.swift
@@ -9,26 +9,25 @@ import SwiftUI
 import MapKit
 
 struct View3: View {
+    
+    /* establishing mutable variable for
+     geographic location of user. @State means
+     that the variable is mutable and "var"
+     is the class type of the variable*/
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: -121), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    //MKCoordinateRegion establishes rectangular map
+    //reigion in specific latitue and longitude
     var body: some View
     {
-        /* establishing mutable variable for
-         geographic location of user. @State means
-         that the variable is mutable and "var"
-         is the class type of the variable*/
-        @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: -121, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)),
-        //MKCoordinateRegion establishes rectangular map
-        //reigion in specific latitue and longitude
-        
-        
         //establish backdrop color
-        Color.black.ignoresSafeArea().overlay(
+        //Color.black.ignoresSafeArea().overlay(
             
         //establing map
-            Map(coordinateRegion: $region),
+            Map(coordinateRegion: $region, showsUserLocation: true)
         VStack
         {
             Text("Map placeholder").foregroundColor(.white)
-        })
+        }//)
     }
 }
 
