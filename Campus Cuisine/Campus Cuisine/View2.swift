ff9365e9e8b7bb8b1beb31ee037cce17dab6cc6e
neubauerseth <neubau23@msu.edu>
2023-08-21 10:59:16 -0400

    Updated the Search bar page (View 2)
    Tried a different way to create the search bar system basically the same as the old way.

--- Campus Cuisine/Campus Cuisine/View2.swift
+++ Campus Cuisine/Campus Cuisine/View2.swift
@@ -1,13 +1,73 @@
 //
 //  View2.swift
 //  Campus Cuisine
-// Link to video: https://www.youtube.com/watch?v=ReHwsu_fjKs
+// Link to OLD video: https://www.youtube.com/watch?v=ReHwsu_fjKs
+//
+// Link to Video: https://www.youtube.com/watch?v=m36HMne-Dqo
 //
 //  //
 
 import SwiftUI
 import UIKit
 
+struct View2: View {
+    
+    @State private var searchText: String = ""
+    
+    private var searchResults: [EmojiDetails]
+    {
+        let results = EmojiProvider.all()
+        if searchText.isEmpty { return results }
+        return results.filter
+        {
+            $0.name.lowercased().hasPrefix(searchText.lowercased()) ||
+            $0.emoji.hasPrefix(searchText)
+        }
+    }
+    
+    var body: some View {
+        NavigationView
+        {
+            List(searchResults) { emojiDetails in
+                NavigationLink(destination: {
+                    EmojiDetailsView(emojiDetails: emojiDetails)
+                })
+                {
+                    Text("\(emojiDetails.emoji) \(emojiDetails.name)")
+                        .padding(6)
+                }
+            }
+            .navigationTitle("Emoji Search")
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "search for emoji"
+            )
+        }
+    }
+}
+
+struct EmojiDetailsView: View
+{
+    let emojiDetails: EmojiDetails
+    
+    var body: some View {
+        HStack
+        {
+            VStack(alignment: .leading)
+            {
+                Text("\(emojiDetails.emoji) \(emojiDetails.name)")
+                    .font(.largeTitle)
+                    .bold()
+                Text(emojiDetails.description)
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding([.leading, .trailing])
+    }
+}
+/*
 struct View2: View {
     //establishing class instance for the restaurant name crap
     @State public var myname = uhidk.shared
@@ -58,7 +118,7 @@ struct View2: View {
             }
         }
     
-}
+}*/
 
 
 
