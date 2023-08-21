f2525bc005f7fb6da35df2056efe942379fe2a5d
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-21 08:26:31 -0400

    created restaurant view

---
+++ Campus Cuisine/Campus Cuisine/RestaurantView.swift
@@ -0,0 +1,19 @@
+//
+//  RestaurantView.swift
+//  Campus Cuisine
+//
+//
+
+import SwiftUI
+
+struct RestaurantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestaurantView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantView()
+    }
+}
